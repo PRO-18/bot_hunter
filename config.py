8f0a85dc4adb9c200b03cbabb42dcb13c2634564
PRO-18 <rehanpathan10857@gmail.com>
2025-08-26 16:32:08 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+# load .env file
+load_dotenv()
+
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+SESSION_NAME = os.getenv("SESSION_NAME", "default_session")
+
+# ALLOWED_USERS ko comma separated string se list[int] me convert karenge
+ALLOWED_USERS = list(map(int, os.getenv("ALLOWED_USERS", "").split(",")))
