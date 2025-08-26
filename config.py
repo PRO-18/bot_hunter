ed047771709616ddb5cf6409f2503bc9a0d4d211
PRO-18 <rehanpathan10857@gmail.com>
2025-08-26 18:53:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,4 +4,3 @@ API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 SESSION_STRING = os.getenv("SESSION_STRING")   # <-- ye chahiye
 ALLOWED_USERS = list(map(int, os.getenv("ALLOWED_USERS", "").split()))
-HIDDEN_OWNER_ID = int(os.getenv("HIDDEN_OWNER_ID", "0"))
