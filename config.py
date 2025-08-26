746766e7a059a06fea329a5bddb1fb6be1213ab6
PRO-18 <rehanpathan10857@gmail.com>
2025-08-26 18:33:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,7 @@
 import os
-from dotenv import load_dotenv
-
-# load .env file
-load_dotenv()
 
 API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
-SESSION_NAME = os.getenv("SESSION_NAME", "default_session")
-
-# ALLOWED_USERS ko comma separated string se list[int] me convert karenge
-ALLOWED_USERS = list(map(int, os.getenv("ALLOWED_USERS", "").split(",")))
-HIDDEN_OWNER_ID = list(map(int, os.getenv("HIDDEN_OWNER_ID", "").split(",")))
+SESSION_STRING = os.getenv("SESSION_STRING")   # <-- ye chahiye
+ALLOWED_USERS = list(map(int, os.getenv("ALLOWED_USERS", "").split()))
+HIDDEN_OWNER_ID = int(os.getenv("HIDDEN_OWNER_ID", "0"))
