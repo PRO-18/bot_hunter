c8e4d1e9d63c9eef27c408b80ff7bba8afefa379
PRO-18 <rehanpathan10857@gmail.com>
2025-08-28 03:47:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,9 @@ import os
 
 API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
-SESSION_STRING = os.getenv("SESSION_STRING")   # <-- ye chahiye
+
+# 🔹 Multiple session strings (comma separated in ENV)
+SESSION_STRINGS = os.getenv("SESSION_STRINGS", "").split(",")
+
+# 🔹 Allowed users (space separated IDs in ENV)
 ALLOWED_USERS = list(map(int, os.getenv("ALLOWED_USERS", "").split()))
